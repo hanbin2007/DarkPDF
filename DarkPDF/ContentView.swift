15134f614c79b471cde13065e43409f2eca2640e
ZhangHanbin <thebestzhq@gmail.com>
2025-08-05 20:07:34 +0800

    Initial Commit

---
+++ DarkPDF/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DarkPDF
+//
+//  Created by zhb on 2025/8/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DarkPDF/DarkPDFApp.swift
@@ -0,0 +1,17 @@
+//
+//  DarkPDFApp.swift
+//  DarkPDF
+//
+//  Created by zhb on 2025/8/5.
+//
+
+import SwiftUI
+
+@main
+struct DarkPDFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
